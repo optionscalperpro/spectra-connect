84dd5fe8de0b9942f00acba061f25a5effe75aaa
Joji <joji@Jojis-MacBook-Air.local>
2025-06-08 05:50:34 +0400

    Add homepage route to dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('', views.admin_dashboard, name='home'),  # This makes / point to the dashboard
 ]
